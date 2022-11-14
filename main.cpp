6b5b6a9cb09ca5d06e35552dc697c5080d312922
mamkad <89218729+mamkad@users.noreply.github.com>
2022-11-14 10:24:40 +0300

    добавление комментариев

--- main.cpp
+++ main.cpp
@@ -10,41 +10,49 @@ using std::atoi;
 bool check_file_extension(string const&);
 
 int main(int argc, char** argv) {
+    // check count of argument
     if (argc != 6) {
         cout << "\n\tWrong count of arguments\n";
         return -1;
     }
 
+    // check file extention
     string fname_matrleft = argv[1], fname_matrright = argv[2], fname_out = argv[3];
     if ( (!check_file_extension(fname_matrleft)) || (!check_file_extension(fname_matrright)) || (!check_file_extension(fname_out)) ) {
         return -1;
     }
-
+    
+    // convert thread count from str to int and check
     int thread_count = atoi(argv[4]);
     if ( !thread_count || thread_count < 0 ) {
         cout << "\n\tWrong input of thread count\n";
         return -1;
     }
 
+    // convert thread sleep from str to int and check
     int sleep_sec = atoi(argv[5]);
     if (!sleep_sec || sleep_sec < 0) {
         cout << "\n\tWrong input of sleep interval\n";
         return -1;
     }
-
+    
+    // load matrixes from files
     matrix leftmatr, rightmatr;
     if ( (!leftmatr.readfromfile(fname_matrleft)) || (!rightmatr.readfromfile(fname_matrright)) ) {
         return -1;
     }
-
+    
+    // matr mult
     matrix resmatr;
     matrix_multiplication matrix_mult(thread_count, sleep_sec);
     if (!matrix_mult.multiply(leftmatr, rightmatr, resmatr)) {
         return -1;
     }
     
+    // print result matrix
     resmatr.print();
 
+     // write result matrix to file end (fname_out)
     if (!resmatr.writetofile(fname_out)) {
         return -1;
     }
