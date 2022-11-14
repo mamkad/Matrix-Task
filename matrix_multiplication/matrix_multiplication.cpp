794a02d3dcb5a2b60fc211ce275ce39e9d911810
mamkad <89218729+mamkad@users.noreply.github.com>
2022-11-14 11:10:54 +0300

    Добавление комментариев

--- matrix_multiplication/matrix_multiplication.cpp
+++ matrix_multiplication/matrix_multiplication.cpp
@@ -7,6 +7,7 @@ using std::cout;
 using std::string;
 using std::cref;
 
+// constructor by number of threads and thread timeout in seconds
 matrix_multiplication::matrix_multiplication(size_t thread_count, size_t sleep_sec) : thread_count_(thread_count),  sleep_sec_(1000 * sleep_sec) {
 }
 
@@ -26,54 +27,65 @@ size_t matrix_multiplication::get_sleep_sec() const noexcept {
     return sleep_sec_;
 }
 
+ // multiply left matrix by right
 bool matrix_multiplication::multiply(matrix const& leftmatr, matrix const& rightmatr, matrix& resmatr) {
+    // checking that the multiplication operation makes sense for such matrices (NxK KxM)
     if (leftmatr.colls() != rightmatr.rows()) {
         cout << "\n\tWrong size of matrixes\n";
         return false;
     }
-
+    
+    // resize result matrix
     resmatr_.resize(leftmatr.rows(), rightmatr.colls()); 
   
     // creating threads
     for (size_t i = 0; i < thread_count_; ++i) {
         threads_mult.push_back(thread(&matrix_multiplication::mult, this, cref(leftmatr), cref(rightmatr)));
     }
-  
-    // wait
+   
+    // wait threads
     for(auto& thd : threads_mult) {
         thd.join();
     }
-
+    
+    // clear threads vector and save resull in resmatr
     threads_mult.clear();
     resmatr = resmatr_;
 
     return true;
 }
 
+// multiplication function for threads
 void matrix_multiplication::mult(matrix const& leftmatr, matrix const& rightmatr) {
-    while (true) {
-
-        mtx_.lock();
+    // thread loop
+    while (true) { 
+        mtx_.lock(); 
+        // calculate the i and j elements of the new matrix, which will calculate
         size_t row = 0, coll = 0;
+        // check that not all elements have been calculated yet
         if (count_of_calc_element < resmatr_.size()) {
-            row  = count_of_calc_element / resmatr_.rows();
+             // ij
+            row  = count_of_calc_element / resmatr_.rows(); 
             coll = count_of_calc_element % resmatr_.colls();
             mtx_.unlock();
         } else {
             mtx_.unlock();
             break;
         }
-
-        matrix_element_t total = 0;
-        size_t common = leftmatr.colls();
-        for (size_t k = 0; k < common; ++k) {
+        
+        // calculating the value of the new element
+        matrix_element_t total = 0;   // value of new element
+        size_t K = leftmatr.colls();  // (NxK KxM)
+        for (size_t k = 0; k < K; ++k) {
             total += leftmatr.at(row, k) * rightmatr.at(k, coll);
         }
         resmatr_.at(row, coll) = total;
         
+        // increase the counter of calculated elements
         ++count_of_calc_element; // atomic
-
+        
+        // sleep
         std::this_thread::sleep_for(std::chrono::milliseconds(sleep_sec_));
-        //cout << std::this_thread::get_id() << ' ' << count_of_calc_element << '\n';
+        // cout << std::this_thread::get_id() << ' ' << count_of_calc_element << '\n';
     }
 }
