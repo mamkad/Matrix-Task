1e03e4e245fbc3d408c2f623785f7e89d18dac2f
mamkad <89218729+mamkad@users.noreply.github.com>
2022-11-14 10:42:30 +0300

    добавление комментариев

--- matrix/matrix.cpp
+++ matrix/matrix.cpp
@@ -14,6 +14,7 @@ using std::ifstream;
 using std::ios_base;
 using std::setw;
 
+// reset params
 void matrix::reset() {
    buff_ = nullptr;
    rows_ = 0;
@@ -21,12 +22,13 @@ void matrix::reset() {
    buffsize_ = 0;
 }
 
+// constructor by rows and colls
 matrix::matrix(size_t r, size_t c) {
     resize(r, c);
 }
 
 matrix::~matrix() {
-    clear();
+    clear(); // deleting content
 }
 
 matrix::matrix(matrix const& m) {
@@ -37,38 +39,39 @@ matrix::matrix(matrix const& m) {
 matrix::matrix(matrix&& m) {
     rows_ = m.rows_;
     colls_ = m.colls_;
-	buffsize_ = m.buffsize_;
-	buff_ = m.buff_;
-	m.reset();
+    buffsize_ = m.buffsize_;
+    buff_ = m.buff_;
+    m.reset();
 }
 
 matrix& matrix::operator= (matrix const& m) {
     if (this == &m) {
-		return *this;
-	}
+	return *this;
+    }
     resize(m.rows_, m.colls_);
-	copy_n(m.buff_, buffsize_, buff_);
+    copy_n(m.buff_, buffsize_, buff_);
     return *this;
 }
 
 matrix& matrix::operator= (matrix && m) {
     if (this == &m) {
-		return *this;
-	}
+	return *this;
+    }
     clear();
     rows_ = m.rows_;
-	colls_ = m.colls_;
-	buffsize_ = m.buffsize_;
-	buff_ = m.buff_;
-	m.reset();
+    colls_ = m.colls_;
+    buffsize_ = m.buffsize_;
+    buff_ = m.buff_;
+    m.reset();
     return *this;
 }
 
+// clear matrix content
 void matrix::clear() {
-    if (isempty()) {
+    if (isempty()) { // if matrix is empty return
         return;
     }
-    delete[] buff_; // ()_
+    delete[] buff_; // 
     reset();
 }
 
@@ -76,6 +79,7 @@ bool matrix::isempty() const noexcept {
     return (buff_ == nullptr);
 }
 
+// resize matrix. if matrix was not empty, old content is deleting
 void matrix::resize(size_t r, size_t c) {
     clear();
     rows_ = r;
@@ -84,10 +88,12 @@ void matrix::resize(size_t r, size_t c) {
     buff_ = new matrix_element_t[buffsize_];
 }
 
+// get matrix element by i and j without check
 matrix_element_t& matrix::at(size_t i, size_t j) {
     return buff_[i * colls_ + j];
 }
 
+// get matrix element by i and j without check
 matrix_element_t const& matrix::at(size_t i, size_t j) const {
     return buff_[i * colls_ + j];
 }
@@ -100,22 +106,26 @@ size_t matrix::rows() const noexcept {
     return rows_;
 }
 
+// size of buffer (rows_ * colls_)
 size_t  matrix::size()  const noexcept {
     return buffsize_;
 }
 
 bool matrix::readfromfile(string const& fnamein) {
     ifstream fin(fnamein);
-
+	
+    // if file is not open
     if (!fin) {
         return false;
     }
 
+    // the first two numbers in the file are interpreted as rows and columns of the matrix
     size_t rows, colls;
     if ( !(fin >> rows >> colls) ) {
         return false;
     }
-
+    
+    // allocating buffer and writing remaining  numbers from file to allocated buffer
     resize(rows, colls);
     for(size_t i = 0; i < rows_; ++i) {
         for(size_t j = 0; j < colls_; ++j) {   
@@ -131,11 +141,13 @@ bool matrix::readfromfile(string const& fnamein) {
 
 bool matrix::writetofile(string const& fnameout) {
     ofstream fout(fnameout, ios_base::app);
-
+    
+    // if file is not open
     if (!fout) {
         return false;
     }
-
+    
+    // convert matrix conent to str 
     ostringstream stream;
     for(size_t i = 0, stri = 0; i < rows_; ++i) {
         for(size_t j = 0; j < colls_; ++j) {
@@ -146,9 +158,12 @@ bool matrix::writetofile(string const& fnameout) {
                  stream << '\n';
             }
         }
-    }
+    } 
+	
+    // add end line
     stream << "---------------------------------------------------------------------------\n";
-
+    
+    // write to file
     fout << stream.str();
     fout.close();
 
